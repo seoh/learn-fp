cd65f882098c17471d152b40d04541f0a9f7d823
Seoh Char <devthewild@gmail.com>
2018-05-08 17:12:44 +0900

    solve monoid

--- src/main/scala/learnfp/monoid/PairAdditiveMonoid.scala
+++ src/main/scala/learnfp/monoid/PairAdditiveMonoid.scala
@@ -3,11 +3,12 @@ package learnfp.monoid
 case class Pair[A, B](a:A, b:B)
 
 object PairAdditiveMonoid {
+  import MonoidOps._
 
   implicit def nestedMonoidInstance[A, B](implicit aMonoid:Monoid[A], bMonoid:Monoid[B]):Monoid[Pair[A, B]] =
     new Monoid[Pair[A, B]] {
-      override def mzero: Pair[A, B] = ???
+      override def mzero: Pair[A, B] = Pair(aMonoid.mzero, bMonoid.mzero)
       override def mappend(lhs: Pair[A, B], rhs: Pair[A, B]): Pair[A, B] =
-        ???
+        Pair(lhs.a |+| rhs.a, lhs.b |+| rhs.b)
   }
 }
