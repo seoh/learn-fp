5d905f010152a5016b122a37e6eeff7e90696f92
Seoh Char <devthewild@gmail.com>
2018-05-08 17:07:46 +0900

    solve typeclass

--- src/main/scala/learnfp/typeclass/Eq.scala
+++ src/main/scala/learnfp/typeclass/Eq.scala
@@ -18,14 +18,17 @@ object EqOps {
 
 object EqInstances {
   implicit val intEqInstance = new Eq[Int] {
-    override def eq(lhs: Int, rhs: Int): Boolean = ???
+    override def eq(lhs: Int, rhs: Int): Boolean = lhs == rhs
   }
 
   implicit val stringEqInstance = new Eq[String] {
-    override def eq(lhs: String, rhs: String): Boolean = ???
+    override def eq(lhs: String, rhs: String): Boolean = lhs == rhs
   }
 
   implicit def listEqInstance[A](implicit eqt:Eq[A]) = new Eq[List[A]] {
-    override def eq(lhs: List[A], rhs: List[A]): Boolean = ???
+    override def eq(lhs: List[A], rhs: List[A]): Boolean =
+      (lhs.size == rhs.size) && ((lhs zip rhs) forall {
+        case (l, r) => eqt.eq(l, r)
+      })
   }
 }
